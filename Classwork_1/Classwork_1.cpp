ee6c5e032d8d1169e7524dd3dd6913c1fb6f3918
CaptainShepard01 <CaptainShepard01@users.noreply.github.com>
2020-02-05 08:26:31 +0200

    Добавьте файлы проекта.

---
+++ Classwork_1/Classwork_1.cpp
@@ -0,0 +1,29 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <fstream>
+#include <direct.h>
+#include <cstdlib>
+using namespace std;
+struct dict {
+	char angl[100];
+	char ukr[100];
+};
+void fun() {
+	ifstream f("words.bin", ios::binary);
+	dict d;
+	_chdir("Ductionary");
+	//_mkdir("words");
+
+	while (f.read((char*)&d, sizeof(dict))) {
+		ofstream f1(d.angl, ios::binary);
+		f1 << d.ukr;
+		f1.close(); cout << "\nCreated\n";
+	}
+	f.close();
+}
+
+int main()
+{
+	
+	return 0;
+}
